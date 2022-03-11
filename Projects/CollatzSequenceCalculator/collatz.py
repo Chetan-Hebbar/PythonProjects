bf7a99ce03096353cbc940ad47a5577ce56d116e
Chetan-Hebbar <chethebbar@gmail.com>
2022-03-10 21:55:40 -0500

    Add some comments

--- Projects/CollatzSequenceCalculator/collatz.py
+++ Projects/CollatzSequenceCalculator/collatz.py
@@ -1,11 +1,13 @@
 import sys
 
+#This is the collatz operator. Divide by 2 for even numbers, and 3x+1 for odd numbers.
 def collatz(number):
     if number % 2==0:
         return number // 2
     if number % 2==1:
         return (3*number) + 1
 
+# Kept the main driver in a while loop, to allow for retries when the input exception is caught
 while True:
     try:
         num=int(input('Enter number:\n'))
