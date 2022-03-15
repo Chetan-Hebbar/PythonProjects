f9faf2f52501a58202066a53e921adb875e2de13
Chetan-Hebbar <chethebbar@gmail.com>
2022-03-14 22:34:44 -0400

    Build function to check for the arbitrary streak length, with some test calls

--- Projects/HeadsTailsStreaks/HeadsTailsStreaks.py
+++ Projects/HeadsTailsStreaks/HeadsTailsStreaks.py
@@ -3,6 +3,14 @@ import random
 numberOfStreaks = 0
 
 
+def StreakChecker(genlist, streaklength, i):
+    refvalue = genlist[i]
+    for n in range(1, streaklength):
+        if genlist[i + n] != refvalue:
+            return False
+    return True
+
+
 def CreateHeadsTailsList(length):
     SampleList = []
     for i in range(length):
@@ -13,10 +21,16 @@ def CreateHeadsTailsList(length):
             list.append(SampleList, 'T')
     return (SampleList)
 
-def CheckForStreaks(List,Length):
-    for i in range(0,len(List)-Length+1):
-        print(str(i)+ ', '+ List[i+Length-1])
+
+def CheckForStreaks(genlist, StreakLength):
+    for i in range(0, len(genlist) - StreakLength + 1):
+        # Check List[i] through List[i+StreakLength-1]
+        isStreak = StreakChecker(genlist, StreakLength, i)
+        # print(str(i)+ ', '+ List[i+Length-1])
+        if isStreak == True:
+            return isStreak
+    return False
 
 
-List=(CreateHeadsTailsList(100))
-print(CheckForStreaks(List,10))
+GeneratedList = (CreateHeadsTailsList(100))
+print(CheckForStreaks(['T', 'H', 'H', 'H', 'H', 'H', 'T', 'H', 'T', 'T'], 99))
