8788f0ff9e5810fd3e5151d9030bda728eeb9d73
Chetan-Hebbar <chethebbar@gmail.com>
2022-04-03 20:28:38 -0400

    Output saved to same local directory as template, as generic save name

--- Projects/MadLibs/madLibs.py
+++ Projects/MadLibs/madLibs.py
@@ -1,4 +1,4 @@
-import re,pathlib,pyinputplus
+import re,pathlib,os
 
 
 def GrabPlaceholders(templatestr):
@@ -16,17 +16,30 @@ def RecordUserEntries(placeholderlist):
         responselist += [response]
     return responselist
 
+def GetTemplateFromUser():
+    while True:
+        user_input = input("Enter the path of your MadlibTemplate: ")
+        pathaddress=pathlib.Path(user_input)
+        if not os.path.exists(pathaddress):
+            print("Couldn't find the file!")
+            continue
+        break
+    return user_input
 
 # Overall Steps:
-# Read reference template file. TODO: Improvement, offer a couple different generic choices for the user to choose
 
-# Reference file will live in C:/Users/Chetan
-TemplateFile = open('C://Users//Chetan//MadlibTemplates//Template1.txt')
-TemplateContents = TemplateFile.read()
+# Validate that the madlib file has tokens to replace
+while True:
+    filePath = GetTemplateFromUser()
+    TemplateFile = open(filePath)
+    TemplateContents = TemplateFile.read()
 
-# Grab the list of inputs we need from the template file
-placeholderlist = GrabPlaceholders(TemplateContents)
-
+    # Grab the list of inputs we need from the template file
+    placeholderlist = GrabPlaceholders(TemplateContents)
+    if len(placeholderlist) ==0 :
+        print("Your file didn't have any MadLib tokens.\n")
+        continue
+    break
 # Prompt the user for inputs
 
 inputlist = RecordUserEntries(placeholderlist)
@@ -42,5 +55,9 @@ print('Here\'s your Madlib:\n' + TemplateContents)
 TemplateFile.close()
 
 # Write that contents of the filled somewhere, and tell the user where they can reference it.
-output=pathlib.Path('C:/Users/Chetan/MadlibTemplates/output.txt')
+# TODO: Ask user what the filename should be called?
+writedirectory=os.path.dirname(filePath)
+output=pathlib.Path(writedirectory / pathlib.Path('output.txt'))
 output.write_text(TemplateContents)
+
+print("Output saved to"+str(output))
